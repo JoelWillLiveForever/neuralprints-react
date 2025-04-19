c5048043cdc1c637efcfbf1cd36e932249765cf3
Vladimir Nikulin <mail.jorey@gmail.com>
2025-04-19 16:02:17 +0300

    Фикс бага, снижающего производительность из-за Tabler Icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
   // server: {
   //   historyApiFallback: true, Чтобы Vite не давал 404 на маршруты
   // },
