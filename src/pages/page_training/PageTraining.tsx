6f9e40624fc182537023122b49503fb0e4676048
Vladimir Nikulin <mail.jorey@gmail.com>
2025-04-22 19:25:42 +0300

    Обновление ручки на странице Training

--- src/pages/page_training/PageTraining.tsx
+++ src/pages/page_training/PageTraining.tsx
@@ -1,9 +1,57 @@
-// import
+import { useEffect, useRef, useState } from 'react';
+import { ImperativePanelHandle, Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import { LazyLog, ScrollFollow } from '@melloware/react-logviewer';
+
+import './page_training.scss';
+
+const DEFAULT_WIDTH_PX = 700;
+const MIN_WIDTH_PX = 600;
+const MAX_WIDTH_PX = 800;
 
 const PageTraining = () => {
-    return(
-        <h1>Training Page</h1>
-    )
-}
+    const containerRef = useRef<HTMLDivElement>(null);
+    const chartPanelRef = useRef<ImperativePanelHandle>(null);
 
-export default PageTraining
+    const getContainerWidth = () => containerRef.current?.getBoundingClientRect().width || window.innerWidth;
+
+    const [chartDefaultSize, setChartDefaultSize] = useState((DEFAULT_WIDTH_PX / getContainerWidth()) * 100);
+    const [chartMinSize, setChartMinSize] = useState((MIN_WIDTH_PX / getContainerWidth()) * 100);
+    const [chartMaxSize, setChartMaxSize] = useState((MAX_WIDTH_PX / getContainerWidth()) * 100);
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = getContainerWidth();
+            setChartDefaultSize((DEFAULT_WIDTH_PX / width) * 100);
+            setChartMinSize((MIN_WIDTH_PX / width) * 100);
+            setChartMaxSize((MAX_WIDTH_PX / width) * 100);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return (
+        <div className="page-training-container" ref={containerRef}>
+            <PanelGroup autoSaveId="persistence" direction="horizontal">
+                <Panel defaultSize={100 - (chartPanelRef.current?.getSize() || chartMaxSize)}>
+                    <div className="log-view-wrapper">
+                        <ScrollFollow
+                            startFollowing={true}
+                            render={({ follow, onScroll }) => (
+                                <LazyLog url="http://example.log" stream follow={follow} onScroll={onScroll} />
+                            )}
+                        />
+                    </div>
+                </Panel>
+
+                <PanelResizeHandle className="my-custom-resize-handle my-custom-resize-handle--training" />
+
+                <Panel defaultSize={chartDefaultSize} minSize={chartMinSize} maxSize={chartMaxSize} ref={chartPanelRef}>
+                    {/* chart view panel content here */}
+                </Panel>
+            </PanelGroup>
+        </div>
+    );
+};
+
+export default PageTraining;
